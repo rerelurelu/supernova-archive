0e198c1362979979bb81f69b1df2e55224deaeb4
rerelurelu <awaawapurin@gmail.com>
2023-08-11 10:55:18 +0900

    layout: Fix height

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 import { component$ } from '@builder.io/qwik';
 import { Link, useLocation } from '@builder.io/qwik-city';
 import { ENTRIES } from '~/const/entries';
-import { cva } from '~/styled-system/css';
+import { css, cva } from '~/styled-system/css';
 import { getPathname } from '~/utils/getPathname';
 
 export default component$(() => {
@@ -10,12 +10,12 @@ export default component$(() => {
   const pathname = getPathname(loc.url.pathname);
 
   return (
-    <header class={headerRecipe()}>
-      <ul class={ulRecipe()}>
+    <header class={header}>
+      <ul class={ul}>
         {ENTRIES.map(({ href, content }) => {
           return (
             <li key={content}>
-              <Link href={href} id={content} class={linkRecipe()}>
+              <Link href={href} id={content} class={link}>
                 <span
                   class={gradationRecipe(content === pathname ? { visual: 'active' } : undefined)}
                 >
@@ -30,47 +30,40 @@ export default component$(() => {
   );
 });
 
-const headerRecipe = cva({
-  base: {
-    width: '100%',
-    minH: '4rem',
-    alignItems: 'center',
-    height: '5rem',
-    top: '0',
-    left: '0',
-    right: '0',
-    pt: '1rem',
-    px: { base: '0', md: '3rem' },
-    zIndex: '50',
-    display: 'flex',
-    justifyContent: { base: 'center', md: 'end' },
-    bg: 'bgHeader',
-    position: 'fixed',
-    backdropFilter: 'blur(8px)',
-  },
+const header = css({
+  width: '100%',
+  alignItems: 'center',
+  height: '4rem',
+  top: '0',
+  left: '0',
+  right: '0',
+  pt: '1rem',
+  px: { base: '0', md: '3rem' },
+  zIndex: '50',
+  display: 'flex',
+  justifyContent: { base: 'center', md: 'end' },
+  bg: 'bgHeader',
+  position: 'fixed',
+  backdropFilter: 'blur(8px)',
 });
 
-const ulRecipe = cva({
-  base: {
-    width: 'max-content',
-    display: 'inline-flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    listStyle: 'none',
-    margin: '0',
-    padding: '0',
-  },
+const ul = css({
+  width: 'max-content',
+  display: 'inline-flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  listStyle: 'none',
+  margin: '0',
+  padding: '0',
 });
 
-const linkRecipe = cva({
-  base: {
-    bg: { _hover: 'transparent', _focus: 'transparent' },
-    gap: '0.75rem',
-    padding: '0.75rem 1rem',
-    color: { base: 'white' },
-    alignItems: 'center',
-    userSelect: 'none',
-  },
+const link = css({
+  bg: { _hover: 'transparent', _focus: 'transparent' },
+  gap: '0.75rem',
+  padding: '0.75rem 1rem',
+  color: { base: 'white' },
+  alignItems: 'center',
+  userSelect: 'none',
 });
 
 const gradationRecipe = cva({
