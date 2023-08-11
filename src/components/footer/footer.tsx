446122cb4bf790a85c7573b90c37d8a69057807f
rerelurelu <awaawapurin@gmail.com>
2023-08-11 11:00:21 +0900

    refactor: Use css func

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,17 +3,17 @@ import { css } from '~/styled-system/css';
 
 export default component$(() => {
   return (
-    <footer
-      class={css({
-        borderTop: '1px solid #312e81',
-        py: '3rem',
-        bg: 'bgBase',
-        letterSpacing: 'widest',
-        color: '#bdc6e9',
-        textAlign: 'center',
-      })}
-    >
+    <footer class={footer}>
       <p>© 2022 relu</p>
     </footer>
   );
+});
+
+const footer = css({
+  borderTop: '1px solid #312e81',
+  py: '3rem',
+  bg: 'bgBase',
+  letterSpacing: 'widest',
+  color: '#bdc6e9',
+  textAlign: 'center',
 });
