8628b2d2f07ee2c5b372076ca527685f0c3dd876
rerelurelu <awaawapurin@gmail.com>
2023-08-15 11:53:00 +0900

    ✨ feat: Add Hero component

---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,69 @@
+import { component$ } from '@builder.io/qwik';
+import { css } from '~/styled-system/css';
+
+export default component$(() => {
+  return (
+    <div class={mainContainer}>
+      <div class={letter}>
+        <span>□ ■</span>
+        <span>■ ■</span>
+        <span>□ ■</span>
+      </div>
+      <div class={letter}>
+        <span>■ □</span>
+        <span>□ ■</span>
+        <span>□ □</span>
+      </div>
+      <div class={letter}>
+        <span>■ □</span>
+        <span>■ □</span>
+        <span>■ □</span>
+      </div>
+      <div class={letter}>
+        <span>■ ■</span>
+        <span>□ □</span>
+        <span>□ □</span>
+      </div>
+      <div class={letter}>
+        <span>■ □</span>
+        <span>□ ■</span>
+        <span>■ □</span>
+      </div>
+      <div class={letter}>
+        <span>■ ■</span>
+        <span>□ □</span>
+        <span>■ □</span>
+      </div>
+      <div class={letter}>
+        <span>■ □</span>
+        <span>□ ■</span>
+        <span>□ □</span>
+      </div>
+      <div class={letter}>
+        <span>□ □</span>
+        <span>■ ■</span>
+        <span>■ □</span>
+      </div>
+    </div>
+  );
+});
+
+const mainContainer = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  flexDir: 'row',
+  columnGap: '3rem',
+  rowGap: '1rem',
+  flexWrap: 'wrap',
+  w: '100%',
+  h: '70vh',
+  pt: { base: '8rem', md: '16rem' },
+});
+
+const letter = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDir: 'column',
+});
