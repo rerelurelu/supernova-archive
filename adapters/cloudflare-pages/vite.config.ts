e6a8ef63559fdd1d3005c8354351deea2053c5cd
rerelurelu <awaawapurin@gmail.com>
2023-08-21 18:48:04 +0900

    🧰 chore: Fix cloudflare settings

--- adapters/cloudflare-pages/vite.config.ts
+++ adapters/cloudflare-pages/vite.config.ts
@@ -1,4 +1,4 @@
-import { vercelEdgeAdapter } from "@builder.io/qwik-city/adapters/vercel-edge/vite";
+import { cloudflarePagesAdapter } from "@builder.io/qwik-city/adapters/cloudflare-pages/vite";
 import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
 
@@ -7,18 +7,17 @@ export default extendConfig(baseConfig, () => {
     build: {
       ssr: true,
       rollupOptions: {
-        input: ["src/entry.vercel-edge.tsx", "@qwik-city-plan"],
+        input: ["src/entry.cloudflare-pages.tsx", "@qwik-city-plan"],
       },
-      outDir: ".vercel/output/functions/_qwik-city.func",
     },
     plugins: [
-      vercelEdgeAdapter({
+      cloudflarePagesAdapter({
         ssg: {
           include: ['/*'],
           origin: 'https://relu.pages.dev',
           sitemapOutFile: 'sitemap.xml',
         },
-      }),
+      })
     ],
   };
 });
